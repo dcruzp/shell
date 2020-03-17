ffee7fa8883a5ab0bf4129d2e1cc795460b307a6
RubenD <rubendariopm14@gmail.com>
2020-03-17 13:26:30 -0400

    added a few things

--- shell.c
+++ shell.c
@@ -5,132 +5,144 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/wait.h>
-#define max_args 13 
-#define maxln_Com_Amb 105 
+#define max_args 13
+#define maxln_Com_Amb 105
 
+char comando[maxln_Com_Amb];
+char *args[max_args];
+int cantcmd = 0;
 
-char comando [maxln_Com_Amb];
-char *args [max_args]; 
-int cantcmd = 0 ; 
+char SHELL[maxln_Com_Amb];
+char PATH[maxln_Com_Amb];
+char HOME[maxln_Com_Amb];
+char PWD[maxln_Com_Amb];
 
-char SHELL [maxln_Com_Amb]; 
-char PATH [maxln_Com_Amb]; 
-char HOME [maxln_Com_Amb]; 
-char PWD [maxln_Com_Amb] ;
+void separaArgs(void);
 
-void separaArgs (void); 
-
-
-/*estas son las funciones a ejecutar */ 
-void execute_cd (void ); 
-void execute_exit (void); 
+/*estas son las funciones a ejecutar */
+void execute_cd(void);
+void execute_exit(void);
 void execute_dir_or_ls(void);
-void execute_pwd(void) ; 
+void execute_pwd(void);
 void execute_echo(void);
-void execute_clear(void); 
+void execute_clear(void);
 /*----------------------------------*/
 
-void listaDir (void); 
-void eco (void) ; 
-void comExterno(void); 
+void listaDir(void);
+void eco(void);
+void comExterno(void);
 
-
-/*estas son los flags del programa */ 
+/*estas son los flags del programa */
 int continuar = 1;
 /*---------------------------------*/
 
-
 int main(int argc, char const *argv[])
 {
-	 
 
-	getcwd (PWD,maxln_Com_Amb); 
-	strcpy(PATH,getenv("PATH")); 
+	getcwd(PWD, maxln_Com_Amb);
+	strcpy(PATH, getenv("PATH"));
 	//printf("%s\n", PATH);
 
-	strcpy(HOME,PWD);
-	strcpy(SHELL,PWD); 
+	strcpy(HOME, PWD);
+	strcpy(SHELL, PWD);
 
+	do
+	{
+		// Print beggining of shell
+		printf("%s>", PWD);
 
-	
-	do {
-		printf("%s>",PWD );
+		// Read input
 		__fpurge(stdin);
+		memset(comando, '\0', maxln_Com_Amb);
+		scanf("%[^\n]s", comando);
 
-		memset(comando,'\0',maxln_Com_Amb);
-		scanf("%[^\n]s" ,comando) ; 
+		// React to input
+		if (strlen(comando) > 0)
+		{
+			separaArgs();
 
-		if (strlen(comando)>0) {
-			separaArgs(); 
-
-			if (strcmp (comando, "cd")==0 )execute_cd();
-			else if (strcmp(comando,"exit")==0)execute_exit();
-			else if (strcmp(comando,"dir") ==0 || strcmp(comando, "ls")==0 )execute_dir_or_ls(); 
-			else if (strcmp(comando,"pwd") ==0 ) execute_pwd () ; 
-			else if (strcmp(comando,"echo")==0 ) execute_echo() ; 
-			else if (strcmp(comando,"clear")==0) execute_clear();
-			else printf("%s\n", "No se reconoce como un comando interno ");
-		}		
-	}while(continuar);
-
-
-	
+			if (strcmp(comando, "cd") == 0)
+				execute_cd();
+			else if (strcmp(comando, "exit") == 0)
+				execute_exit();
+			else if (strcmp(comando, "dir") == 0 || strcmp(comando, "ls") == 0)
+				execute_dir_or_ls();
+			else if (strcmp(comando, "pwd") == 0)
+				execute_pwd();
+			else if (strcmp(comando, "echo") == 0)
+				execute_echo();
+			else if (strcmp(comando, "clear") == 0)
+				execute_clear();
+			else
+				printf("%s\n", "No se reconoce como un comando interno ");
+		}
+	} while (continuar);
 
 	return 0;
 }
 
-
-void separaArgs (void){
-	int i ; 
-	for (i=0 ; i < max_args-1; i++) args[i]= NULL; 
-	strtok(comando , " ") ; 
-	i=0 ; 
-	args[i] =comando ;
-	while((args[++i]= strtok(NULL," "))!=NULL&& i <(max_args-2)); 
-	cantcmd = i ; 
+void separaArgs(void)
+{
+	int i;
+	for (i = 0; i < max_args - 1; i++)args[i] = NULL;
+	
+	i = 0;
+	args[i] = strtok(comando, " ");
+	if(!args[i])return;
+	
+	while ((args[++i] = strtok(NULL, " ")) != NULL && i < (max_args - 1))
+		;
+	cantcmd = i;
 }
 
-
-
-
-void execute_cd  (void ){
+void execute_cd(void)
+{
 	if (args[1])
-		if (chdir(args[1])!=0)
+		if (chdir(args[1]) != 0)
 			printf("Error '%s'  no existe o np se puede cambiar a este directorio\n", args[1]);
-		else getcwd(PWD,maxln_Com_Amb); 
+		else
+			getcwd(PWD, maxln_Com_Amb);
 }
 
-void execute_exit (void){
-	continuar = 0; 
+void execute_exit(void)
+{
+	continuar = 0;
 }
 
-void execute_dir_or_ls(void){
-	char ruta [maxln_Com_Amb] ; 
-	int archs ; 
-	int cnt = -1; 
-	struct dirent **lista ; 
-	strcpy (ruta,PWD); 
-	if (args[1]) strcat (ruta , "/"),strcat (ruta ,args[1]) ; 
-	archs = scandir(ruta , &lista ,0,alphasort) ; 
-	if (archs <0 )
-		printf("Error , no existe o no se puede leer [%s]\n",ruta);
-	else if (archs == 2 ){
-		//solo encontro los directorios [ . , .. ] 
+void execute_dir_or_ls(void)
+{
+	char ruta[maxln_Com_Amb];
+	int archs;
+	int cnt = -1;
+	struct dirent **lista;
+	strcpy(ruta, PWD);
+	if (args[1])
+		strcat(ruta, "/"), strcat(ruta, args[1]);
+	archs = scandir(ruta, &lista, 0, alphasort);
+	if (archs < 0)
+		printf("Error , no existe o no se puede leer [%s]\n", ruta);
+	else if (archs == 2)
+	{
+		//solo encontro los directorios [ . , .. ]
 	}
-	else {
-		while(++cnt < archs ){
-			if (strcmp(lista[cnt]->d_name, ".")!=0 && strcmp(lista[cnt]->d_name, "..")!=0)
+	else
+	{
+		while (++cnt < archs)
+		{
+			if (strcmp(lista[cnt]->d_name, ".") != 0 && strcmp(lista[cnt]->d_name, "..") != 0)
 				printf("%s\n", lista[cnt]->d_name);
 		}
 	}
 }
 
-void execute_pwd (void) {
+void execute_pwd(void)
+{
 	printf("%s\n", PWD);
 }
 
-void execute_echo (void){
-	
+void execute_echo(void)
+{
+
 	/*
 	int  i =0 , maxsize = 64 ;
     char c, * buf ;
@@ -159,50 +171,57 @@ void execute_echo (void){
     }
     */
 
+	if (!args[1])
+		return;
+	int i, j, k = 0;
+	char aux[6];
 
-	if (!args[1]) 
-		return; 
-	int i,j,k =0;
-	char aux [6]; 
-
-
-
-	while(args[++k]){
-		for (i=0 ; i<strlen(args[k]); i++){
-			if (args[k][i]!= '$')printf("%c", args[k][i]);
-			else {
-				j=-1;
-				while(++j<5&&strlen(args[k]) && args[k][i+j+1]!='\0')
-					aux [j] = args[k][i+j+1] ; 
+	while (args[++k])
+	{
+		for (i = 0; i < strlen(args[k]); i++)
+		{
+			if (args[k][i] != '$')
+				printf("%c", args[k][i]);
+			else
+			{
+				j = -1;
+				while (++j < 5 && strlen(args[k]) && args[k][i + j + 1] != '\0')
+					aux[j] = args[k][i + j + 1];
 				aux[j] = '\0';
-				if (strcmp(aux,"SHELL") ==0 )
-					printf("%s\n", SHELL) , i+=5;
-				else if (strncmp(aux,"PATH",4)==0)
-					printf("%s\n", PATH), i+=4;
-				else if (strncmp(aux , "PWD",3)==0)
-					printf("%s\n", PWD) , i+=3;
-				else if (strncmp(aux , "HOME",4) == 0 )
-					printf("%s\n", HOME ), i+=4;
-				else printf("%s\n", "$");
+				if (strcmp(aux, "SHELL") == 0)
+					printf("%s\n", SHELL), i += 5;
+				else if (strncmp(aux, "PATH", 4) == 0)
+					printf("%s\n", PATH), i += 4;
+				else if (strncmp(aux, "PWD", 3) == 0)
+					printf("%s\n", PWD), i += 3;
+				else if (strncmp(aux, "HOME", 4) == 0)
+					printf("%s\n", HOME), i += 4;
+				else
+					printf("%s\n", "$");
 			}
 		}
 		printf(" ");
-	} 
+	}
 	printf("\n");
 }
 
-void execute_clear(void){
-	int pid =0, status ; 
+void execute_clear(void)
+{
+	int pid = 0, status;
 	pid = fork();
-	if (pid<0)printf("%s\n", "Error no se pudo crear un proceso hijo");
-	if (pid==0){
-		status = execvp("clear" , args) ; 
-		if(status){
-			printf("Error!%s no se reconoce o no se puede ejcutar \n", comando );
-			exit(1) ;
-		}		
+	if (pid < 0)
+		printf("%s\n", "Error no se pudo crear un proceso hijo");
+	if (pid == 0)
+	{
+		status = execvp("clear", args);
+		if (status)
+		{
+			printf("Error!%s no se reconoce o no se puede ejcutar \n", comando);
+			exit(1);
+		}
 	}
-	else {
-		wait(NULL) ; 
-	}	
+	else
+	{
+		wait(NULL);
+	}
 }
