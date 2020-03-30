67d3b1924869f0fd2b080b4fe9527c99e0ab8100
Victor Lantigua <v.lantigua94@gmail.com>
2020-03-29 20:15:40 -0400

    Add some comments to Execute.h

--- Execute.h
+++ Execute.h
@@ -2,12 +2,16 @@
 #define EXECUTE
 #include "shell_structures.h"
 
+//Ejecuta una linea de comandos
 int Execute(Command * cmd);
 
+//Devuelve el file descriptor del que va a leer un subproceso
 int GetInfd(subCommand * scmd);
 
+//Devuelve el file descriptor en el que va a ecribirr un subproceso
 int GetOutfd(subCommand * scmd);
 
+//ejecuta un subproceso
 int RunCMD(subCommand * scmd, int fdin, int fdout);
 
 #endif
