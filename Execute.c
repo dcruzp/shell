69e1e7cb2a20ab227681f85b43b44986bdf11885
Victor Lantigua <v.lantigua94@gmail.com>
2020-04-02 17:36:25 -0400

    Added ctrl+c signal handler

--- Execute.c
+++ Execute.c
@@ -7,41 +7,46 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
 
-int Execute(Command * cmd)
+int Contin = 0;
+int Execute(Command *cmd)
 {
-   int fdpipe[2], fdin, fdout, rcmd, status,
-   backupSTDIN = dup(STDIN_FILENO), backupSTDOUT = dup(STDOUT_FILENO);
-   pid_t cpid;
-
-   fdin = GetInfd(&cmd->commands[0]);
-   if(fdin < 0)
-   {
-       return -1;
-   }
+    int fdpipe[2], fdin, fdout, rcmd, status,
+    backupSTDIN = dup(STDIN_FILENO), backupSTDOUT = dup(STDOUT_FILENO);
+    pid_t cpid;
+    ppid = getpid();
+    sigintcount = 0;
+    signal(SIGINT, SIGINTHandler);
+    signal(SIGCHLD, SIGCHLDHandler);
+    signal(SIGTSTP, SIGTSTPHandler);
+    
+    fdin = GetInfd(&cmd->commands[0]);
+    if (fdin < 0)
+    {
+        return -1;
+    }
 
     for (int i = 0; i < cmd->subCommandCount; i++)
     {
-        
 
         fdout = GetOutfd(&cmd->commands[i]);
-        if(fdout < 0)
+        if (fdout < 0)
         {
             return -1;
         }
-       
 
-        if(i < cmd->subCommandCount - 1)
+        if (i < cmd->subCommandCount - 1)
         {
-             int pi = pipe(fdpipe);
-            if(pi < 0)
+            int pi = pipe(fdpipe);
+            if (pi < 0)
             {
                 perror("pipe");
                 return -1;
             }
-            if(fdout == STDOUT_FILENO)
+            if (fdout == STDOUT_FILENO)
             {
-                fdout = fdpipe[1];            
+                fdout = fdpipe[1];
             }
             else
             {
@@ -49,10 +54,16 @@ int Execute(Command * cmd)
             }
         }
 
-        cpid = fork();
-        
-        if(cpid == 0)
+        if(strcmp(cmd->commands[i].cmd,"exit") == 0)
         {
+            return 2;
+        }
+
+        cpid = fork();
+        if (cpid == 0)
+        {
+            signal(SIGINT, SIG_IGN);
+            signal(SIGTSTP, SIG_DFL);
             rcmd = RunCMD(&cmd->commands[i], fdin, fdout);
             if (rcmd < 0)
             {
@@ -60,24 +71,22 @@ int Execute(Command * cmd)
             }
         }
 
-
-        if(fdin != GetInfd(&cmd->commands[i]))
+        if (fdin != GetInfd(&cmd->commands[i]))
         {
             close(fdin);
         }
 
-        if(fdout != GetOutfd(&cmd->commands[i]))
+        if (fdout != GetOutfd(&cmd->commands[i]))
         {
             close(fdout);
         }
 
-        if(i < cmd->subCommandCount - 1)
+        if (i < cmd->subCommandCount - 1)
         {
-            fdin = GetInfd(&cmd->commands[i+1]);
+            fdin = GetInfd(&cmd->commands[i + 1]);
         }
-        
-        
-        if(fdin == STDIN_FILENO)
+
+        if (fdin == STDIN_FILENO)
         {
             fdin = fdpipe[0];
         }
@@ -85,68 +94,76 @@ int Execute(Command * cmd)
         {
             close(fdpipe[0]);
         }
-        
     }
 
-    if(cmd->_background == 0)
+    if (cmd->_background == 0)
     {
-        printf("Inside this guy\n");
-        int waitResult = waitpid(cpid, &status, 0);
-        if(waitResult < 0)
+        wpid = cpid;
+
+        while(1)
         {
-            perror("waitpid");
-            return -1;
+            int status;
+            int out;
+            out = waitpid(cpid, &status, WUNTRACED);
+            if(WIFEXITED(status))
+            {
+                break;
+            }
+            if(WIFSTOPPED(status))
+            {
+                break;
+            }
         }
+        wpid = -1;
     }
     else
     {
-        printf("%d\n", cpid);
+        printf("[%d]\n", cpid);
     }
-    
-    
+
     return 0;
 }
 
-int GetInfd(subCommand * scmd)
+int GetInfd(subCommand *scmd)
 {
     int outFD = STDIN_FILENO;
-    if(scmd->inRcount > 0)
+    if (scmd->inRcount > 0)
     {
         for (int i = 0; i < scmd->inRcount; i++)
         {
             outFD = open(scmd->inR[i], O_RDONLY);
-            if(outFD < 0)
+            if (outFD < 0)
             {
                 perror("Not such a file or directory");
                 return -1;
             }
         }
     }
-    
+
     return outFD;
 }
 
-int GetOutfd(subCommand * scmd)
+int GetOutfd(subCommand *scmd)
 {
     int outFD = STDOUT_FILENO;
-    if(scmd->appRcount > 0)
+    if (scmd->appRcount > 0)
     {
         for (int i = 0; i < scmd->appRcount; i++)
         {
-            outFD = open(scmd->appR[i], O_CREAT | O_APPEND | O_WRONLY);
-            if(outFD < 0)
+            outFD = open(scmd->appR[i], O_CREAT | O_APPEND | O_WRONLY, S_IRWXU);
+            if (outFD < 0)
             {
                 perror("Trying to open for Append");
                 return -1;
             }
         }
     }
-    if(scmd->outRcount > 0)
+    if (scmd->outRcount > 0)
     {
         for (int i = 0; i < scmd->outRcount; i++)
         {
-            outFD = open(scmd->outR[i], O_CREAT | O_WRONLY | O_TRUNC);
-            if(outFD < 0)
+            outFD = open(scmd->outR[i], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+            if (outFD < 0)
             {
                 perror("Trying to open for Writting");
                 return -1;
@@ -157,23 +174,101 @@ int GetOutfd(subCommand * scmd)
     return outFD;
 }
 
-int RunCMD(subCommand * scmd, int fdin, int fdout)
+int RunCMD(subCommand *scmd, int fdin, int fdout)
 {
-    if(fdin != STDIN_FILENO)
+    if (fdin != STDIN_FILENO)
     {
         dup2(fdin, STDIN_FILENO);
         close(fdin);
     }
-    if(fdout != STDOUT_FILENO)
+    if (fdout != STDOUT_FILENO)
     {
         dup2(fdout, STDOUT_FILENO);
         close(fdin);
     }
-
     execvp(scmd->cmd, scmd->args);
     perror("Problem execute a program");
 
-    printf("%s se pasó\n", scmd->cmd);
-
     return -1;
+}
+
+int ExecuteBuitins(subCommand * scmd, int infd, int outfd)
+{
+    if(strcmp(scmd->cmd, JOBS) == 0)
+    {
+        //jobs execution here
+    }
+    else if (strcmp(scmd->cmd, BG) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, FG) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, CD) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, PWD) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, AGAIN) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, HISTORY) == 0)
+    {
+        /* code */
+    }
+    else if (strcmp(scmd->cmd, EXIT) == 0)
+    {
+        /* code */
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+void SIGINTHandler(int in)
+{
+        if(wpid < 0)
+        { 
+            write(STDOUT_FILENO, "\nPrompt $ ", 11);
+            return;
+        }
+        if(sigintcount < 1)
+        { 
+            write(STDOUT_FILENO, "Press ctrl+c again to teminate\n", 32);
+            sigintcount++;
+        }
+        else
+        { 
+            sigintcount = 0; 
+            kill(wpid, SIGKILL);
+            wpid = -1;
+        }
+}
+
+void SIGTSTPHandler(int in)
+{
+    if(wpid == -1)
+    {
+        write(STDOUT_FILENO, "\nPrompt $ ", 11);
+    }
+}
+
+void SIGCHLDHandler(int in)
+{
+    int status;
+    int auxpid = waitpid(wpid, &status, WNOHANG);
+    if(!WIFEXITED(status) && WIFSTOPPED(status))
+    {
+        kill(wpid, SIGSTOP);
+    }
+
+    wpid = -1;
 }
--- Execute.h
+++ Execute.h
@@ -2,6 +2,36 @@
 #define EXECUTE
 #include "shell_structures.h"
 
+#define JOBS "jobs"
+#define BG "bg"
+#define FG "fg"
+#define CD "cd"
+#define PWD "pwd"
+#define AGAIN "again"
+#define HISTORY "history"
+#define EXIT "exit"
+
+ #define BGSIZE 1000000
+
+ //Process background[BGSIZE];
+enum state {Stopped, Running};
+//const char * States[] = {"Stopped", "Running"};
+
+
+int ppid;
+int sigintcount;
+int wpid;
+
+typedef struct Process
+{
+    pid_t ppid;
+    pid_t pid;
+    char * state;
+}Process;
+
+
+int CreateProcess(pid_t pid, pid_t ppid, int state);
+
 //Ejecuta una linea de comandos
 int Execute(Command * cmd);
 
@@ -13,5 +43,11 @@ int GetOutfd(subCommand * scmd);
 
 //ejecuta un subproceso
 int RunCMD(subCommand * scmd, int fdin, int fdout);
+
+void SIGINTHandler(int in);
+
+void SIGTSTPHandler(int in);
+
+void SIGCHLDHandler(int in);
 
 #endif
